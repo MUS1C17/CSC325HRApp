2cb5fab88a203285b757034a24a01dcfdb243684
MUS1C17 <msharovarov@murraystate.edu>
2024-09-18 10:13:51 -0500

    Quick save

---
+++ hrapp/src/main/java/com/hrapp/Employee.java
@@ -0,0 +1,15 @@
+package com.hrapp;
+
+public abstract class Employee {
+
+    protected String firstName;
+    protected String lastName;
+    protected String employeeID;
+    protected String email;
+    protected String phoneNumber;
+    protected double hourlyRate;
+    protected int age;
+
+    
+
+}
