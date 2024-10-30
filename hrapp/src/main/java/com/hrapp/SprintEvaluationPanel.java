ffdd75679f6f6c501c7103ac3e7a284692320a85
Mark Sharovarov <ukrmark5@gmail.com>
2024-10-29 20:36:15 -0500

    Added a navigation panel on the left side of the details. It has 3 buttons Details, Job History and Sprint Evaluation

---
+++ hrapp/src/main/java/com/hrapp/SprintEvaluationPanel.java
@@ -0,0 +1,14 @@
+package com.hrapp;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SprintEvaluationPanel extends JPanel 
+{
+
+    public SprintEvaluationPanel()
+    {
+        JLabel label = new JLabel("Hello, this is the Sprint Evaluation Panel");
+        add(label);
+    }
+}
