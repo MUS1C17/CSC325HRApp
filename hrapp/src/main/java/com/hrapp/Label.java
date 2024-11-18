ff2bd4094476d31d63cc4adaead0a4c05a411533
Mark Sharovarov <ukrmark5@gmail.com>
2024-11-18 00:38:22 -0600

    Small fix

--- hrapp/src/main/java/com/hrapp/Label.java
+++ hrapp/src/main/java/com/hrapp/Label.java
@@ -1,5 +1,4 @@
 package com.hrapp;
-import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
@@ -7,38 +6,55 @@ import java.awt.GraphicsEnvironment;
 import java.io.File;
 import java.io.IOException;
 
-public class Label extends JLabel { //TODO: Set up try-catch with font formatting for new font Montserrat and variants
-    public Label(String text) {
+import javax.swing.JLabel;
+
+public class Label extends JLabel 
+{ //TODO: Set up try-catch with font formatting for new font Montserrat and variants
+    public Label(String text) 
+    {
         super("<html>" + text + "</html>");
-        try {
+        try 
+        {
             Font montserrat = Font.createFont(Font.TRUETYPE_FONT, new File("resources\\fonts\\Montserrat\\static\\Montserrat-Bold.ttf"));
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(montserrat);
             super.setFont(montserrat.deriveFont(Font.PLAIN, 18));
-        } catch(IOException | FontFormatException e) {
+        } 
+        catch(IOException | FontFormatException e) 
+        {
             e.printStackTrace();
         }
     }
-    public Label(String text, int fontSize) {
+
+    public Label(String text, int fontSize) 
+    {
         super("<html>" + text + "</html>");
-        try {
+        try 
+        {
             Font montserrat = Font.createFont(Font.TRUETYPE_FONT, new File("resources\\fonts\\Montserrat\\static\\Montserrat-Bold.ttf"));
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(montserrat);
             super.setFont(montserrat.deriveFont(Font.PLAIN, fontSize));
-        } catch(IOException | FontFormatException e) {
+        } 
+        catch(IOException | FontFormatException e) 
+        {
             e.printStackTrace();
         }
     }
-    public Label(String text, int fontSize, Color color) {
+
+    public Label(String text, int fontSize, Color color) 
+    {
         super("<html>" + text + "</html>");
-        try {
+        try 
+        {
             Font montserrat = Font.createFont(Font.TRUETYPE_FONT, new File("resources\\fonts\\Montserrat\\static\\Montserrat-Bold.ttf"));
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(montserrat);
             super.setFont(montserrat.deriveFont(Font.PLAIN, fontSize));
             super.setForeground(color);
-        } catch(IOException | FontFormatException e) {
+        } 
+        catch(IOException | FontFormatException e) 
+        {
             e.printStackTrace();
         }
     }
--- hrapp/src/main/java/com/hrapp/TextField.java
+++ hrapp/src/main/java/com/hrapp/TextField.java
@@ -1,9 +1,12 @@
 package com.hrapp;
-import javax.swing.JTextField;
 import java.awt.Font;
 
-public class TextField extends JTextField {
-    public TextField() {
+import javax.swing.JTextField;
+
+public class TextField extends JTextField 
+{
+    public TextField() 
+    {
         super();
         super.setFont(new Font("Arial", Font.PLAIN, 16));
     }
